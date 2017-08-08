39bb1db3111c6bd4cba509f06e51d23123245db1
Sai Gopal <git@saigopal.com>
2017-08-08 21:59:22 +0530

    Code Check In

---
+++ js/init.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) {
+    setTimeout(function() {
+	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	 }, 100);
+   $('header').css({ 'height': $(window).height() });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height() });
+        $('body').css({ 'width': $(window).width() })
+   });
+});
+
+
+
+
+
+
+
+
